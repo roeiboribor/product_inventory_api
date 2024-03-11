61e9cf4510c314976ee8daafe079b7c4d74d9542
Roei Boribor <roeiboribor@gmail.com>
2024-03-11 17:03:52 +0800

    minor changes

---
+++ app/Http/Controllers/Api/V1/ApiController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class ApiController extends Controller
+{
+    // Register API (POST)
+    public function register(Request)
+    {
+        
+    }
+}
